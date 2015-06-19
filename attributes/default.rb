8dc80c33ec91967c00a7207f24b6fbab255c71b6
Floyd <kirinse@gmail.com>
2015-06-19 19:16:08 +0800

    - init

---
+++ attributes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: jboss7
+# Attributes:: default
+# Author:: Sascha Moellering
+
+default['jboss']['home'] = '/srv/jboss'
+default['jboss']['path'] = 'wildfly-8.0.0.CR1'
+default['jboss']['version'] = '8.0.0'
+default['jboss']['url'] = 'http://download.jboss.org/wildfly/8.0.0.CR1/wildfly-8.0.0.CR1.zip'
+default['jboss']['tarball'] = 'wildfly-8.0.0.CR1.zip'
+default['jboss']['user'] = 'jboss'
+default['jboss']['application'] = 'jboss'
+default['jboss']['config'] = 'standalone'
+default['jboss']['script'] = 'standalone.sh'
+default['jboss']['manage_config_file'] = true
+
+node.override['java']['jdk_version'] = '7'
+node.override['java']['install_flavor'] = 'oracle'
+node.override['java']['oracle']['accept_oracle_download_terms'] = true
+
+default['aws']['s3']['access_key'] = 'AKIAJ6WH2ZKPR2UESB4A'
+default['aws']['s3']['secret_access_key'] = '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'
+default['aws']['s3']['bucket'] = 'jboss-eap-cluster-config'
